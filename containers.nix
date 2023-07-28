93dc7edd2deb65f683f387285c2fac3eb3da42df
James Baker <james.baker@helsing.ai>
2023-07-28 16:49:55 +0100

    jk

--- containers.nix
+++ containers.nix
@@ -142,7 +142,7 @@ in {
     inherit libnvidia_container0 libnvidia_container1 libnvidia_container_tools;
     # nvidiaContainerRuntime = nvidia_container_toolkit;
     nvidiaContainerRuntime = writeShellScriptBin "nvidia-container-runtime" ''
-      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${lib.makeLibraryPath [libnvidia_container_tools libnvidia_container0]}"
+      export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${lib.makeLibraryPath [libnvidia_container0]}"
       export PATH="$PATH:${nvidia_ctk}/bin:${nvidia_container_runtime_hook}/bin:${docker}/bin:${shadow}/bin"
       exec ${nvidia_container_toolkit}/bin/nvidia-container-runtime "$@"
     '';
