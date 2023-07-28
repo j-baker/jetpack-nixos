b7d38960c70f30a7d43b977e987b5877dc3f4492
James Baker <james.baker@helsing.ai>
2023-07-28 17:59:52 +0100

    jk

--- containers.nix
+++ containers.nix
@@ -11,6 +11,7 @@
   coreutils,
   docker,
   shadow,
+pkg-config, rpcsvc-proto, makeWrapper, removeReferencesTo,
 }:
 
 let
@@ -95,6 +96,100 @@ let
     version = (lib.head (debsForSourcePackage name)).version;
     srcs = builtins.map (deb: deb.src) (debsForSourcePackage name);
   } // args);
+
+  modprobeVersion = "495.44";
+  nvidia-modprobe = fetchFromGitHub {
+    owner = "NVIDIA";
+    repo = "nvidia-modprobe";
+    rev = modprobeVersion;
+    sha256 = "sha256-Y3ZOfge/EcmhqI19yWO7UfPqkvY1CHHvFC5l9vYyGuU=";
+  };
+  modprobePatch = substituteAll {
+    src = ./modprobe.patch;
+    inherit modprobeVersion;
+  };
+
+  libnvidia_container0 = stdenv.mkDerivation rec {
+    pname = "libnvidia-container";
+    version = "0.11.0+jetpack";
+    src = fetchFromGithub {
+      owner = "NVIDIA";
+      repo = "libnvidia-container";
+      rev = "v${version}";
+      sha256 = "sha256-7OAawWwjeKU8wIa8I/+aSvAJli4kEua94nJSNyCajpE=";
+    };
+
+    patches = [
+      ./nvc_ldcache.patch
+    ];
+
+  postPatch = ''
+    sed -i \
+      -e 's/^REVISION ?=.*/REVISION = ${src.rev}/' \
+      -e 's/^COMPILER :=.*/COMPILER = $(CC)/' \
+      mk/common.mk
+
+    mkdir -p deps/src/nvidia-modprobe-${modprobeVersion}
+    cp -r ${nvidia-modprobe}/* deps/src/nvidia-modprobe-${modprobeVersion}
+    chmod -R u+w deps/src
+    pushd deps/src
+
+    patch -p0 < ${modprobePatch}
+    touch nvidia-modprobe-${modprobeVersion}/.download_stamp
+    popd
+
+    # 1. replace DESTDIR=$(DEPS_DIR) with empty strings to prevent copying
+    #    things into deps/src/nix/store
+    # 2. similarly, remove any paths prefixed with DEPS_DIR
+    # 3. prevent building static libraries because we don't build static
+    #    libtirpc (for now)
+    # 4. prevent installation of static libraries because of step 3
+    # 5. prevent installation of libnvidia-container-go.so twice
+    sed -i Makefile \
+      -e 's#DESTDIR=\$(DEPS_DIR)#DESTDIR=""#g' \
+      -e 's#\$(DEPS_DIR)\$#\$#g' \
+      -e 's#all: shared static tools#all: shared tools#g' \
+      -e '/$(INSTALL) -m 644 $(LIB_STATIC) $(DESTDIR)$(libdir)/d' \
+      -e '/$(INSTALL) -m 755 $(libdir)\/$(LIBGO_SHARED) $(DESTDIR)$(libdir)/d'
+  '';
+
+  enableParallelBuilding = true;
+
+  preBuild = ''
+    HOME="$(mktemp -d)"
+  '';
+
+  env.NIX_CFLAGS_COMPILE = toString [ "-I${libtirpc.dev}/include/tirpc" ];
+  NIX_LDFLAGS = [ "-L${libtirpc.dev}/lib" "-ltirpc" ];
+
+  nativeBuildInputs = [ pkg-config rpcsvc-proto makeWrapper removeReferencesTo ];
+
+  buildInputs = [ libelf libcap libseccomp libtirpc ];
+
+  makeFlags = [
+    "WITH_LIBELF=yes"
+    "prefix=$(out)"
+    # we can't use the WITH_TIRPC=yes flag that exists in the Makefile for the
+    # same reason we patch out the static library use of libtirpc so we set the
+    # define in CFLAGS
+    "CFLAGS=-DWITH_TIRPC"
+  ];
+
+  postInstall =
+    let
+      inherit (addOpenGLRunpath) driverLink;
+      libraryPath = lib.makeLibraryPath [ "$out" driverLink "${driverLink}-32" ];
+    in
+    ''
+      wrapProgram $out/bin/nvidia-container-cli --prefix LD_LIBRARY_PATH : ${libraryPath}
+    '';
+  };
+  
+   buildFromDebs {
+    name = "libnvidia-container0";
+    buildInputs = [ libcap libseccomp ];
+  };
+
   libnvidia_container0 = buildFromDebs {
     name = "libnvidia-container0";
     buildInputs = [ libcap libseccomp ];
