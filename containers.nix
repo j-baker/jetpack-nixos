c8740dc1c6653d06241fba49bac1736f2da292dd
james.baker@helsing.ai <james.baker@helsing.ai>
2023-07-30 11:58:49 +0100

    j

--- containers.nix
+++ containers.nix
@@ -58,65 +58,63 @@ let
     patches = [
       ./nvc-ldcache.patch
       ./avoid-static-libtirpc-build.patch
-      ./patch8.patch
-      ./patch11.patch
-      ./patch13.patch
+      ./nixos-base.patch
     ];
-  postPatch = ''
-    sed -i \
-      -e 's/^REVISION :=.*/REVISION = ${src.rev}/' \
-      -e 's/^COMPILER :=.*/COMPILER = $(CC)/' \
-      mk/common.mk
+    postPatch = ''
+      sed -i \
+        -e 's/^REVISION :=.*/REVISION = ${src.rev}/' \
+        -e 's/^COMPILER :=.*/COMPILER = $(CC)/' \
+        mk/common.mk
 
-    sed -i 's#/etc/nvidia-container-runtime/host-files-for-container.d#${l4tCsv}#g' src/nvc_info.c
-    sed -i 's#NIXOS_BASE#${unpackedDebs}#g' src/jetson_mount.c
-    sed -i 's#NIXOS_BASE#${unpackedDebs}#g' src/nvc_info.c
+      sed -i 's#/etc/nvidia-container-runtime/host-files-for-container.d#${l4tCsv}#g' src/nvc_info.c
+      sed -i 's#NIXOS_BASE#${unpackedDebs}#g' src/jetson_mount.c
+      sed -i 's#NIXOS_BASE#${unpackedDebs}#g' src/nvc_info.c
 
-    mkdir -p deps/src/nvidia-modprobe-${modprobeVersion}
-    cp -r ${nvidia-modprobe}/* deps/src/nvidia-modprobe-${modprobeVersion}
-    chmod -R u+w deps/src
-    pushd deps/src
+      mkdir -p deps/src/nvidia-modprobe-${modprobeVersion}
+      cp -r ${nvidia-modprobe}/* deps/src/nvidia-modprobe-${modprobeVersion}
+      chmod -R u+w deps/src
+      pushd deps/src
 
-    # patch -p0 < ${modprobePatch}
-    touch nvidia-modprobe-${modprobeVersion}/.download_stamp
-    popd
+      # patch -p0 < ${modprobePatch}
+      touch nvidia-modprobe-${modprobeVersion}/.download_stamp
+      popd
 
-    # 1. replace DESTDIR=$(DEPS_DIR) with empty strings to prevent copying
-    #    things into deps/src/nix/store
-    # 2. similarly, remove any paths prefixed with DEPS_DIR
-    # 3. prevent building static libraries because we don't build static
-    #    libtirpc (for now)
-    # 4. prevent installation of static libraries because of step 3
-    # 5. prevent installation of libnvidia-container-go.so twice
-    sed -i Makefile \
-      -e 's#DESTDIR=\$(DEPS_DIR)#DESTDIR=""#g' \
-      -e 's#\$(DEPS_DIR)\$#\$#g' \
-      -e 's#all: shared static tools#all: shared tools#g' \
-      -e '/$(INSTALL) -m 644 $(LIB_STATIC) $(DESTDIR)$(libdir)/d' \
-      -e '/$(INSTALL) -m 755 $(libdir)\/$(LIBGO_SHARED) $(DESTDIR)$(libdir)/d'
-  '';
+      # 1. replace DESTDIR=$(DEPS_DIR) with empty strings to prevent copying
+      #    things into deps/src/nix/store
+      # 2. similarly, remove any paths prefixed with DEPS_DIR
+      # 3. prevent building static libraries because we don't build static
+      #    libtirpc (for now)
+      # 4. prevent installation of static libraries because of step 3
+      # 5. prevent installation of libnvidia-container-go.so twice
+      sed -i Makefile \
+        -e 's#DESTDIR=\$(DEPS_DIR)#DESTDIR=""#g' \
+        -e 's#\$(DEPS_DIR)\$#\$#g' \
+        -e 's#all: shared static tools#all: shared tools#g' \
+        -e '/$(INSTALL) -m 644 $(LIB_STATIC) $(DESTDIR)$(libdir)/d' \
+        -e '/$(INSTALL) -m 755 $(libdir)\/$(LIBGO_SHARED) $(DESTDIR)$(libdir)/d'
+    '';
 
-  enableParallelBuilding = true;
+    enableParallelBuilding = true;
 
-  preBuild = ''
-    HOME="$(mktemp -d)"
-  '';
+    preBuild = ''
+      HOME="$(mktemp -d)"
+    '';
 
-  NIX_CFLAGS_COMPILE = toString [ "-I${libtirpc.dev}/include/tirpc" ];
-  NIX_LDFLAGS = [ "-L${libtirpc}/lib" "-ltirpc" ];
+    NIX_CFLAGS_COMPILE = toString [ "-I${libtirpc.dev}/include/tirpc" ];
+    NIX_LDFLAGS = [ "-L${libtirpc}/lib" "-ltirpc" ];
 
-  nativeBuildInputs = [ pkg-config rpcsvc-proto ];
+    nativeBuildInputs = [ pkg-config rpcsvc-proto ];
 
-  buildInputs = [ libelf libcap libseccomp libtirpc ];
+    buildInputs = [ libelf libcap libseccomp libtirpc ];
 
-  makeFlags = [
-    "WITH_LIBELF=yes"
-    "prefix=$(out)"
-    # we can't use the WITH_TIRPC=yes flag that exists in the Makefile for the
-    # same reason we patch out the static library use of libtirpc so we set the
-    # define in CFLAGS
-    "CFLAGS=-DWITH_TIRPC"
-  ];
+    makeFlags = [
+      "WITH_LIBELF=yes"
+      "prefix=$(out)"
+      # we can't use the WITH_TIRPC=yes flag that exists in the Makefile for the
+      # same reason we patch out the static library use of libtirpc so we set the
+      # define in CFLAGS
+      "CFLAGS=-DWITH_TIRPC"
+    ];
   };
 in {
     inherit libnvidia_container0;
