209b5d83a029939287fc59972d59a49cf10c34b8
james.baker@helsing.ai <james.baker@helsing.ai>
2023-07-30 08:37:15 +0100

    jk

--- containers.nix
+++ containers.nix
@@ -127,7 +127,7 @@ let
 
   devicesCsv = pkgs.runCommand "devices-l4t.csv" {} ''
     mkdir -p "$out"
-    grep "^dev," "${origL4tXml}" > "$out/l4t.csv"
+    grep "^dev," "${origL4tCsv}" > "$out/l4t.csv"
   '';
 
   # Pass to libnvidia_container0.
